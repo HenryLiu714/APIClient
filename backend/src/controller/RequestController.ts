dc902d0aa87ef6beff1bce5261e09a23a18fd0ce
HenryLiu714 <henryliu714@gmail.com>
2026-03-30 22:05:01 -0400

    second

---
+++ backend/src/controller/RequestController.ts
@@ -0,0 +1,7 @@
+export interface IRequestController {
+
+}
+
+class RequestController implements IRequestController {
+
+}
---
+++ shared/types/schemas.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const RequestSchema = z.object({
+    id: z.uuid(),
+    url: z.url(),
+    parameters: z.record(z.string(), z.any()).optional(),
+    method: z.enum(['GET', 'POST', 'DELETE', 'PUT']),
+    headers: z.record(z.string(), z.string()).optional(),
+    body: z.string().optional()
+})
+
+export type APIRequest = z.infer<typeof RequestSchema>
